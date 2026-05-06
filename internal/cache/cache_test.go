d99984e8f1c7fe847efb091b975591d661c985fe
Aymane D. Hlamnach <dara-@outlook.jp>
2026-05-06 20:04:24 +0100

    〜
    Signed-off-by: Aymane D. Hlamnach <dara-@outlook.jp>

--- internal/cache/cache_test.go
+++ internal/cache/cache_test.go
@@ -161,17 +161,14 @@ func TestCache_LRU(t *testing.T) {
 	err = c.Set("key4", bigVal, "")
 	assert.NoError(t, err)
 
-	var evictErr error
-	assert.Eventually(t, func() bool {
-		var d io.ReadCloser
-		d, _, _, evictErr = c.Get("key1")
-		if d != nil {
-			_ = d.Close()
-		}
-		return evictErr == ErrNotFound
-	}, 2*time.Second, 10*time.Millisecond)
+	select {
+	case <-c.triggerCh:
+	default:
+	}
+	c.evictLoop()
 
-	assert.Equal(t, ErrNotFound, evictErr)
+	_, _, _, err = c.Get("key1")
+	assert.Equal(t, ErrNotFound, err)
 
 	data, _, _, err = c.Get("key0")
 	assert.NoError(t, err)
